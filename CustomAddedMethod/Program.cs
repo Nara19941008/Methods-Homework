6e13b7dd4f5198b6142310ccaeb6eeeb9f27795f
Nara19941008 <narminim@code.edu.az>
2022-07-14 08:55:50 +0400

    Added bio table

---
+++ CustomAddedMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using CustomAddedMethod.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CustomAddedMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyLIst<string> studentList = new MyLIst<string>();
+
+            //studentList.Add("Narmin");
+            //studentList.Add("fidan");
+            //studentList.Add("Seadet");
+            //studentList.Add("Emil");
+
+
+            //studentList.Clear("Fidan");
+
+            //studentList.GetAll();
+
+
+
+        }
+    }
+    
+}
